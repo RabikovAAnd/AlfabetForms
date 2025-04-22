90ad848a6919dc1b668c8c2a7daba50addcbc2fc
RabikovAAnd <randr.one@yandex.ru>
2025-04-22 11:48:51 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBoxSource = New System.Windows.Forms.ListBox()
+        Me.ListBoxTarget = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ButtonCheck = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBoxSource
+        '
+        Me.ListBoxSource.FormattingEnabled = True
+        Me.ListBoxSource.ItemHeight = 16
+        Me.ListBoxSource.Location = New System.Drawing.Point(30, 50)
+        Me.ListBoxSource.Name = "ListBoxSource"
+        Me.ListBoxSource.Size = New System.Drawing.Size(150, 180)
+        Me.ListBoxSource.TabIndex = 0
+        '
+        'ListBoxTarget
+        '
+        Me.ListBoxTarget.AllowDrop = True
+        Me.ListBoxTarget.FormattingEnabled = True
+        Me.ListBoxTarget.ItemHeight = 16
+        Me.ListBoxTarget.Location = New System.Drawing.Point(250, 50)
+        Me.ListBoxTarget.Name = "ListBoxTarget"
+        Me.ListBoxTarget.Size = New System.Drawing.Size(150, 180)
+        Me.ListBoxTarget.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Исходный список"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(250, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(128, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Отсортируйте здесь"
+        '
+        'ButtonCheck
+        '
+        Me.ButtonCheck.Location = New System.Drawing.Point(160, 250)
+        Me.ButtonCheck.Name = "ButtonCheck"
+        Me.ButtonCheck.Size = New System.Drawing.Size(120, 30)
+        Me.ButtonCheck.TabIndex = 4
+        Me.ButtonCheck.Text = "Проверить"
+        Me.ButtonCheck.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 303)
+        Me.Controls.Add(Me.ButtonCheck)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBoxTarget)
+        Me.Controls.Add(Me.ListBoxSource)
+        Me.Name = "Form1"
+        Me.Text = "Расположи по алфавиту"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBoxSource As ListBox
+    Friend WithEvents ListBoxTarget As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ButtonCheck As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Dim draggedItem As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Заполнение исходного списка случайными словами
+        Dim words As String() = {"Апельсин", "Банан", "Груша", "Виноград", "Яблоко"}
+        Dim rnd As New Random()
+        For Each word In words.OrderBy(Function(w) rnd.Next())
+            ListBoxSource.Items.Add(word)
+        Next
+    End Sub
+
+    Private Sub ListBoxSource_MouseDown(sender As Object, e As MouseEventArgs) Handles ListBoxSource.MouseDown
+        If ListBoxSource.SelectedItem IsNot Nothing Then
+            draggedItem = ListBoxSource.SelectedItem.ToString()
+            DoDragDrop(draggedItem, DragDropEffects.Move)
+        End If
+    End Sub
+
+    Private Sub ListBoxTarget_DragEnter(sender As Object, e As DragEventArgs) Handles ListBoxTarget.DragEnter
+        If e.Data.GetDataPresent(DataFormats.Text) Then
+            e.Effect = DragDropEffects.Move
+        End If
+    End Sub
+
+    Private Sub ListBoxTarget_DragDrop(sender As Object, e As DragEventArgs) Handles ListBoxTarget.DragDrop
+        Dim item As String = e.Data.GetData(DataFormats.Text).ToString()
+        If Not ListBoxTarget.Items.Contains(item) Then
+            ListBoxTarget.Items.Add(item)
+            ListBoxSource.Items.Remove(item)
+        End If
+    End Sub
+
+    Private Sub ButtonCheck_Click(sender As Object, e As EventArgs) Handles ButtonCheck.Click
+        Dim sorted = ListBoxTarget.Items.Cast(Of String)().OrderBy(Function(w) w).ToArray()
+        Dim original = ListBoxTarget.Items.Cast(Of String)().ToArray()
+
+        If sorted.SequenceEqual(original) Then
+            MessageBox.Show("Правильно! Слова отсортированы по алфавиту.")
+        Else
+            MessageBox.Show("Неправильно! Попробуйте ещё раз.")
+        End If
+    End Sub
+End Class
